f2f4ca1b36b3c7e80a4a11fb38c7be29ed5d028c
ksaweryg <ksawery.gie@gmail.com>
2021-01-17 13:41:06 +0100

    zadanie10.py

---
+++ zadanie10.py
@@ -0,0 +1,19 @@
+import re
+
+bridge_of_death = '''
+-Jaki jest twój ulubiony kolor?
+-Niebieski!
+-Dobrze. Idź.
+...
+-Stój. Jakie jest twe imię?
+-Sir Galahad z Camelotu.
+-Jaki jest twój cel?
+-Odnaleźć Graala.
+-Jaki jest twój ulubiony kolor?
+-Niebieski... nie... żóóóółtyyyy!
+'''
+
+
+pattern1 = re.compile(r"\b\w{4,}\b")
+result1 = pattern1.findall(bridge_of_death)
+print(result1)
